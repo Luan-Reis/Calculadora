1f8fa6b8ec0d6fe39e35f6767af921d66a45101e
Luan-Reis <luanreis600@gmail.com>
2022-08-09 19:57:33 -0300

    Adicionar as operações básicas #3

--- calculadora.py
+++ calculadora.py
@@ -4,3 +4,15 @@ number_2 = int(input('Enter your second number: '))
 
 print('{} + {} = '.format(number_1, number_2))
 print(number_1 + number_2)
+
+
+print('{} - {} = '.format(number_1, number_2))
+print(number_1 - number_2)
+
+
+print('{} * {} = '.format(number_1, number_2))
+print(number_1 * number_2)
+
+
+print('{} / {} = '.format(number_1, number_2))
+print(number_1 + number_2)
