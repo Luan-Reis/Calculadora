e669639adedbc832bf5c88279ed82e71b45f70c7
Luan_Reis <luanreis600@gmail.com>
2022-08-09 19:20:30 -0300

    #2 Implementar a adição de dois números

---
+++ calculadora.py
@@ -0,0 +1,6 @@
+number_1 = int(input('Enter your first number: '))
+number_2 = int(input('Enter your second number: '))
+
+
+print('{} + {} = '.format(number_1, number_2))
+print(number_1 + number_2)
